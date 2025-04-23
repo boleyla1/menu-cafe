9a4521d6aacbeded591ee76d0a578a1fa93a0e55
mehrshad boleyla <boleyla.mehrshad1@gmail.com>
2025-04-24 03:27:55 +0330

    model for Order

--- cart_app/admin.py
+++ cart_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from  . import models
 
+class OrderItemAdmin(admin.TabularInline):
+    model = models.OrderItem
 # Register your models here.
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'phone','is_paid')
+    inlines = [OrderItemAdmin]
+    list_filter = ('is_paid',)
--- cart_app/models.py
+++ cart_app/models.py
@@ -3,8 +3,22 @@ from accounts.models import User
 from cafe_app.models import Product
 
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.phone
 
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='items')
+    quantity = models.SmallIntegerField()
+    price = models.PositiveIntegerField()
 
-
-# Create your models here.
+    def __str__(self):
+        return self.product.name
